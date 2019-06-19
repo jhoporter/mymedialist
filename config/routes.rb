a3a2ce484998ad7b8a7520ac121e2ff420ddeb73
Jho Porter <jhoanaporter@gmail.com>
2019-06-18 21:02:14 -0400

    added a homepage by generating a Pages controller, adding a home method and view. also added code to delete medium when rake db:seed is run

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/' => 'pages#home'
   get 'views' => 'views#index'
   get 'media' => 'media#index'
   get 'views/new' => 'views#new'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Medium.delete_all
 Medium.create([
   {
     title: 'John Wick 3',
